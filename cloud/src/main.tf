811fd11506f4da16b50467fbb3dba151c8860d49
David <25187726+david-shortman@users.noreply.github.com>
2023-11-16 23:49:20 -0500

    Revert "use framework"
    This reverts commit 2b14c7f09dd3e34dd07aa89e96b5cc987d6f510b.

--- cloud/src/main.tf
+++ cloud/src/main.tf
@@ -55,7 +55,6 @@ resource "aws_amplify_app" "test_app" {
   EOT
 
   platform = "WEB_COMPUTE"
-  framework = "Next.js - SSR"
 
   # The default rewrites and redirects added by the Amplify Console.
 #  custom_rule {
