0e737432bd7916a0dd789132343f3b0819486e40
David <25187726+david-shortman@users.noreply.github.com>
2023-11-16 19:27:50 -0500

    test

---
+++ cloud/src/main.tf
@@ -0,0 +1,34 @@
+resource "aws_amplify_app" "test_app" {
+  name       = "test_app"
+  repository = "https://github.com/example/app"
+
+  build_spec = <<-EOT
+    version: 0.1
+    frontend:
+      phases:
+        preBuild:
+          commands:
+            - yarn install
+        build:
+          commands:
+            - yarn run build
+      artifacts:
+        baseDirectory: build
+        files:
+          - '**/*'
+      cache:
+        paths:
+          - node_modules/**/*
+  EOT
+
+  # The default rewrites and redirects added by the Amplify Console.
+  custom_rule {
+    source = "/<*>"
+    status = "404"
+    target = "/index.html"
+  }
+
+  environment_variables = {
+    ENV = "test"
+  }
+}
